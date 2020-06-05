1665f4c50a41d4c3547854281ad9e3d1473d99bc
AlbertinoDias <36789123+AlbertinoDias@users.noreply.github.com>
2020-06-05 04:24:03 -0700

    minor fisicas e cena apra saves
    meti a funç\ao principla do sistema de fisica mas n\ao está igual ao da stora
    
    também comecei a meter coisas para os saves, aidna tenho de perceber como é que as views e assim funcionam tho

--- CutleryAdventures_IOSVersion/CutleryAdventures_IOSVersion/GameScene.swift
+++ CutleryAdventures_IOSVersion/CutleryAdventures_IOSVersion/GameScene.swift
@@ -1,12 +1,47 @@
 import SpriteKit
 import GameplayKit
 
-// dependendia da
+var recordData:String!
+
+
+struct PhysicsCategory {
+    static let none : UInt32 = 0
+    static let all  : UInt32 = UInt32.max
+    static let grandpa : UInt32 = 0b1 // 1
+    static let floor : UInt32 = 0b10 // 2
+}
+
 class GameScene: SKScene, SKPhysicsContactDelegate {
     
     override func didMove(to view: SKView) {
         // define a cor do fundo base
         backgroundColor = SKColor.gray
     }
+    
+}
+
+func saveScore(){
+    if(recordData == nil)
+    {
+        let savedString = "place holder"
+        let userDefaults = Foundation.UserDefaults.standard
+        userDefaults.set(savedString, forKey: "BestScore")
+        
+    }
+    else
+    {
+        //let score:Int? = Int(scorelable.text!)
+        //let record:Int? = Int(recordData)
+        //if(score! > record!){
+          //  let savedString = "place holder"
+            //let userDefaults = Foundation.UserDefaults.standard
+            //userDefaults.set(savedString, forKey: "BestScore")        }
+        
+    }
+    
+    
+    
+    
+    
     
 }
--- CutleryAdventures_IOSVersion/CutleryAdventures_IOSVersion/GameViewController.swift
+++ CutleryAdventures_IOSVersion/CutleryAdventures_IOSVersion/GameViewController.swift
@@ -10,6 +10,8 @@ import UIKit
 import SpriteKit
 import GameplayKit
 
+
+
 class GameViewController: UIViewController {
 
     override func viewDidLoad() {
@@ -48,4 +50,18 @@ class GameViewController: UIViewController {
     override var prefersStatusBarHidden: Bool {
         return true
     }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        let userDefaults = Foundation.UserDefaults.standard
+        let value = userDefaults.string(forKey: "BestScore")
+        
+        if(value == nil) {
+        //label.text = "0"
+        }
+        else{
+        //lable.text = value
+        }
+        
+    }
+    
 }
